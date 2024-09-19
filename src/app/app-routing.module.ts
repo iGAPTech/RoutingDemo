0454f4640506480c4b702407c59b124c27a6b1e8
iGAP Tech <kishor@igaptechnologies.com>
2024-09-19 16:55:03 +0530

    Demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"home", component:HomeComponent},
+  {path:"about", component:AboutComponent},
+  {path:"services", component:ServicesComponent},
+  {path:"contact", component:ContactComponent},
+  {path:"**", component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
